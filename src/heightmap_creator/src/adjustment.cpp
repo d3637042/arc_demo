316c324921e263fb7240541d592edff1e39c15f5
Daniel <d3637042@yahoo.com.tw>
2017-12-26 15:46:09 +0800

    warp heightmap

--- src/heightmap_creator/src/adjustment.cpp
+++ src/heightmap_creator/src/adjustment.cpp
@@ -7,11 +7,11 @@
 using namespace cv;
 using namespace std;
 
-Mat src; Mat src_gray;
+Mat src, src2; Mat src_gray;
 int thresh = 8;
 int max_thresh = 255;
 RNG rng(12345);
-
+Mat warp_mat( 2, 3, CV_32FC1 );
 /// Function header
 void thresh_callback(int, void* );
 
@@ -20,7 +20,7 @@ int main( int argc, char** argv )
 {
   /// Load source image and convert it to gray
   src = imread( argv[1], 1 );
-
+  src2 = imread( argv[2], 1 );
   /// Convert image to gray and blur it
   cvtColor( src, src_gray, CV_BGR2GRAY );
   blur( src_gray, src_gray, Size(3,3) );
@@ -32,6 +32,17 @@ int main( int argc, char** argv )
 
   thresh_callback(0, 0);
 
+
+  Mat warp_dst, warp_dst2;
+  warpAffine( src, warp_dst, warp_mat, warp_dst.size() );
+  warpAffine( src2, warp_dst2, warp_mat, warp_dst2.size() );
+  char* warp_window = "Warped";
+  cout << warp_dst.size() << endl;
+  namedWindow( warp_window, CV_WINDOW_AUTOSIZE );
+  imshow( warp_window, warp_dst );
+
+  imwrite("heightmapcolor.jpg", warp_dst);
+  imwrite("heightmapimg.jpg", warp_dst2);
   waitKey(0);
   return(0);
 }
@@ -53,7 +64,7 @@ void thresh_callback(int, void* )
   vector<Rect> boundRect( contours.size() );
   vector<Point2f>center( contours.size() );
   vector<float>radius( contours.size() );
-
+  Rect Apriltagbox;
   for( int i = 0; i < contours.size(); i++ )
      { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
        boundRect[i] = boundingRect( Mat(contours_poly[i]) );
@@ -70,8 +81,10 @@ void thresh_callback(int, void* )
        int size = (boundRect[i].br().x-boundRect[i].tl().x)*(boundRect[i].br().y-boundRect[i].tl().y);
        
        if (size>15000 && size<18000){
-	std::cout << "top_left_point:" << boundRect[i].tl() <<" width:" << boundRect[i].br().x-boundRect[i].tl().x << " height:" << boundRect[i].br().y-boundRect[i].tl().y << std::endl;
+		std::cout << "top_left_point:" << boundRect[i].tl() <<" width:" << boundRect[i].br().x-boundRect[i].tl().x << " height:" << boundRect[i].br().y-boundRect[i].tl().y << std::endl;
        	rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+       	Apriltagbox = boundRect[i];
+
        }
        //circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
      }
@@ -79,4 +92,22 @@ void thresh_callback(int, void* )
   /// Show in a window
   namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
   imshow( "Contours", drawing );
+
+  //transform to groundtruth
+  //top_left: 160-65=95 112-65=47 130*130
+  Point2f srcpnts[4];
+  Point2f dstpnts[4];
+  srcpnts[0] = Apriltagbox.tl();
+  srcpnts[1] = Point2f(Apriltagbox.tl().x, Apriltagbox.br().y);
+  srcpnts[2] = Apriltagbox.br();
+  srcpnts[3] = Point2f(Apriltagbox.br().x, Apriltagbox.tl().y);
+  dstpnts[0] = Point2f(95, 47);
+  dstpnts[1] = Point2f(95, 47+130);
+  dstpnts[2] = Point2f(95+130, 47+130);
+  dstpnts[3] = Point2f(95+130, 47);
+
+  
+  warp_mat = getAffineTransform( srcpnts, dstpnts );
+
+
 }
